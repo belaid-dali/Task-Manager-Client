99b34166456c228342dad23b3b322b101129f8fc
Belaid DALI OMAR <belaid.daliomar@dz.gt.com>
2024-01-27 00:24:57 +0100

    adding .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: '/Task-Manager-Client/',
   plugins: [react()],
-})
+  base: '/Task-Manager-Client/', 
+  // ... other options
+});
